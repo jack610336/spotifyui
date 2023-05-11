90f48808695363371b7d83ba1225420fa1160523
Jack_Wang <jack_wang@asus.com>
2023-05-11 13:54:12 +0800

    fix the padding

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -117,7 +117,7 @@ class _HomePageState extends State<HomePage> {
               ),
               child: Padding(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
                 child: Row(
                   children: [
                     Text(
